e34406a08cf01301a09ccfc4a55d131da715462e
Asif <ama85bd@gmail.com>
2025-01-22 08:48:26 +0600

    12

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,10 @@ export default function Home() {
   return (
     <>
       <section className='pink_container'>
-        <h1 className='heading'>Home</h1>
+        <h1 className='heading'>
+          Pitch Your Startup, <br /> Connect With Enterpreneurs{' '}
+        </h1>
+        <p className='sub-heading !max-w-3xl'>Submit Ideas, Vote on Pitches</p>
       </section>
     </>
   );
